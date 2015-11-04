58a986a327a0e273c196b00ea06a0f9d771794d1
Cesar Rodriguez <cesar.rdz.v@gmail.com>
2015-11-04 15:13:30 +0200

    Add correlation functions

--- exercise-1/movielens/exercise.py
+++ exercise-1/movielens/exercise.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 
+import numpy
+
 import loadmovielens
 
 class Exercise(object):
@@ -51,6 +53,46 @@ class Exercise(object):
         user_junction = self.get_union_of_users(movie_a_id, movie_b_id)
 
         return float(len(user_intersection)) / len(user_junction)
+
+    def get_correlation_coefficient_from_titles(self, title_a, title_b):
+        movie_a_ids = loadmovielens.give_me_movie_id(title_a, self.items_dictionary)
+        movie_b_ids = loadmovielens.give_me_movie_id(title_b, self.items_dictionary)
+
+        if len(movie_a_ids) == 0 or len(movie_b_ids) == 0:
+            exit("Movie titles not found")
+        if len(movie_a_ids) > 1 or len(movie_b_ids) > 1:
+            exit("Ambigous search titles")
+
+        return self.get_correlation_coefficient_from_ids(movie_a_ids[0][0], movie_b_ids[0][0])
+
+    def get_correlation_coefficient_from_ids(self, movie_a_id, movie_b_id):
+        # empty ids
+        if not movie_a_id or not movie_b_id:
+            return 0.0
+        # movies do not exist in database
+        elif movie_a_id not in self.movie_to_ratings or movie_b_id not in self.movie_to_ratings:
+            return 0.0
+
+        user_intersection = self.get_intersection_of_users(movie_a_id, movie_b_id)
+        ratings_users_a = []
+
+        for rating in self.movie_to_ratings[movie_a_id]:
+            if rating[0] in user_intersection:
+                ratings_users_a.append(rating)
+
+        ratings_users_b = []
+
+        for rating in self.movie_to_ratings[movie_b_id]:
+            if rating[0] in user_intersection:
+                ratings_users_b.append(rating)
+
+        # we have both rating arrays, now sort them by movie ID so ratings match 1:1
+        sorted(ratings_users_a, key=lambda x: x[1])
+        sorted(ratings_users_b, key=lambda x: x[1])
+        ratings_a = [r[2] for r in ratings_users_a]
+        ratings_b = [r[2] for r in ratings_users_b]
+
+        return numpy.corrcoef(ratings_a, ratings_b)[0][1]
 
     def get_union_of_users(self, movie_a_id, movie_b_id):
         return self.get_users_from_ratings(self.movie_to_ratings[movie_a_id]).union(self.get_users_from_ratings(self.movie_to_ratings[movie_b_id]))
