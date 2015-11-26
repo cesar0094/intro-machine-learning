8459b3652e9e62005688e737293fcd254df8cbf5
Cesar Rodriguez <cesar.rdz.v@gmail.com>
2015-11-26 19:19:59 +0200

    Finish part B

--- exercise-4/main.py
+++ exercise-4/main.py
@@ -11,13 +11,6 @@ K = 10
 def fit_func(x, *args):
     return sum(a * x ** i for i, a in enumerate(args))
 
-def calculate_optimal_weight(X, y):
-    X = numpy.matrix(X)
-    return (X.transpose() * X).inverse() * X.transpose() * y
-
-def generate_matrix(observations, func):
-    return numpy.matrix([ [1] + func(observations) for _ in observations ])
-
 x = random.uniform(-3, 3, NUM_SAMPLES)
 f_x = lambda x: 2 + x - 0.5 * x**2
 
@@ -31,7 +24,7 @@ x_aprox = numpy.arange(-3, 3, 0.2)
 
 for k in range(1, K+2):
 
-    x0 = numpy.zeros(k)
+    x0 = numpy.ones(k)
     values, cov_matrix = optimize.curve_fit(fit_func, x, y, x0)
     y_aprox = [fit_func(i, *values) for i in x_aprox]
 
@@ -51,15 +44,23 @@ n_partitions = int(math.ceil(NUM_SAMPLES/float(FOLD_SIZE)))
 x_partitions = [ x[FOLD_SIZE*i:FOLD_SIZE*(i+1)] for i in range(n_partitions) ]
 y_partitions = [ y[FOLD_SIZE*i:FOLD_SIZE*(i+1)] for i in range(n_partitions) ]
 
-# for k in range(1, K+2):
+error_sums = []
+for k in range(1, K+2):
+    error_sum = 0
+    for i in range(n_partitions):
+        training_x = numpy.array([x_partitions[j] for j in range(n_partitions) if j != i ]).flatten()
+        training_y = numpy.array([y_partitions[j] for j in range(n_partitions) if j != i ]).flatten()
 
-#     for i in range(n_partitions):
-#         training_x = numpy.flatten([x_partitions[i] if j != i for j in range(n_partitions)])
-#         training_y = numpy.flatten([y_partitions[i] if j != i for j in range(n_partitions)])
+        x0 = numpy.zeros(k)
+        values, cov_matrix = optimize.curve_fit(fit_func, training_x, training_y, x0)
+        y_aprox = [fit_func(j, *values) for j in x_partitions[i]]
 
-#         x0 = numpy.zeros(k)
-#         values, cov_matrix = optimize.curve_fit(fit_func, training_x, training_y, x0)
-#         y_aprox = [fit_func(i, *values) for i in x_aprox]
+        sqr_err_sum = sum([ (training_y[i] - fit_func(x_i, *values))**2 for i, x_i in enumerate(training_x) ])
 
-#         sqr_err_sum = sum([ (training_y[i] - fit_func(x_i, *values))**2 for i, x_i in enumerate(training_x) ])
+        error_sum += sqr_err_sum
 
+    error_sums.append(error_sum)
+
+plt.plot(error_sums, color='blue', lw=2)
+plt.yscale('log')
+plt.show()
